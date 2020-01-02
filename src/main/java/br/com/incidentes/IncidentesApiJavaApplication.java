534cbe7ae0687bc16571b7b9603f0a12de4ab996
denner <denner@stj.jus.br>
2020-01-02 13:18:39 -0300

    first commit

---
+++ src/main/java/br/com/incidentes/IncidentesApiJavaApplication.java
@@ -0,0 +1,13 @@
+package br.com.incidentes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncidentesApiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncidentesApiJavaApplication.class, args);
+	}
+
+}
